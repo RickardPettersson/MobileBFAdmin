b88cf1cb0cc83c2d889f7960e26cc62d32bc27d4
Rickard Pettersson <rickardnp@gmail.com>
2011-11-03 20:45:44 +0100

    Added playerlist html page with jquery mobile first version only show playerlist

--- Helpers/WebserviceResponse.cs
+++ Helpers/WebserviceResponse.cs
@@ -27,16 +27,43 @@ namespace BFAdmin.Helpers
             // Try to find path
             if (new Regex("/$").Match(myRequest.URL).Success)
             {
-                answer = @"
-                    <html>
-                        <head>
-                            <title>HEJ</title>
-                        </head>
-                        <body>
-                            <h1>HEJ</h1>
-                        </body>
-                    </html>
-                ";
+                StringBuilder sb = new StringBuilder();
+                sb.Append("<html><head><title>Battlefield 3 Server Admin</title>");
+                sb.Append("<link rel=\"stylesheet\" href=\"http://code.jquery.com/mobile/1.0rc2/jquery.mobile-1.0rc2.min.css\" />");
+                sb.Append("<script src=\"http://code.jquery.com/jquery-1.6.4.min.js\"></script>");
+                sb.Append("<script src=\"http://code.jquery.com/mobile/1.0rc2/jquery.mobile-1.0rc2.min.js\"></script>");
+                sb.Append("</head><body>");
+                sb.Append("<div data-role=\"page\" id=\"playerlist\">");
+                sb.Append("<div data-role=\"header\" data-theme=\"b\"><h1>BF3 Server Admin</h1></div>");
+                sb.Append("<div data-role=\"content\" id=\"one\">");
+                sb.Append("<h2>Playerlist</h2>");
+                sb.Append("<ul data-role=\"listview\" data-inset=\"true\" data-theme=\"d\">");
+                if (Program.Playerlist.Count == 0)
+                {
+                    sb.Append("<li><a href=\"#playertodo\">No players on the server</a></li>");
+                }
+                else
+                {
+                    foreach (var p in Program.Playerlist)
+                    {
+                        sb.Append("<li><a href=\"#\">" + p.Name + "</a></li>");
+                    }
+                }
+                sb.Append("</ul>");
+                sb.Append("</div>");
+                sb.Append("</div>");
+                sb.Append("<div data-role=\"page\" id=\"playertodo\">");
+                sb.Append("<div data-role=\"header\" data-theme=\"b\"><h1>BF3 Server Admin</h1></div>");
+                sb.Append("<div data-role=\"content\" id=\"one\">");
+                sb.Append("<h2>Do with player XXX</h2>");
+                sb.Append("<ul data-role=\"listview\" data-inset=\"true\" data-theme=\"d\">");
+                sb.Append("<li><a href=\"#\">Kick</a></li>");
+                sb.Append("<li><a href=\"#\">Ban</a></li>");
+                sb.Append("</ul>");
+                sb.Append("</div>");
+                sb.Append("</div>");
+                sb.Append("</body></html>");
+                answer = sb.ToString();
             }
             else if (new Regex("/GetStations").Match(myRequest.URL).Success)
             {
